bb91ab2396144cf3fdff187d2026978c4b37ecf4
NilKad <kadulin@gmail.com>
2023-11-15 23:28:40 +0100

    del assets

--- assets/scripts/template-scripts/event.js
+++
@@ -1,1 +0,0 @@
-console.log("event page");const btnMoreRef=document.getElementById("btn-more"),textContentRef=document.querySelector(".event-content");btnMoreRef.addEventListener("click",(e=>{textContentRef.classList.toggle("clamp"),textContentRef.scrollIntoView({behavior:"smooth"})}));
