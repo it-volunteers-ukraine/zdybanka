38f31e26a9a77d5a6e2a743be37550c38f8c5672
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-11-29 18:38:19 +0200

    sec main page

--- src/scripts/template-scripts/home.js
+++ src/scripts/template-scripts/home.js
@@ -1,26 +1,35 @@
 // add swiper
-new Swiper(".gallery-slider", {
-  loop: true,
-  autoHeight: false,
-  watchOverflow: true,
 
+new Swiper(".gallery-slider", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  dynamicBullets: true,
+  dynamicMainBullets: 3,
+  navigation: {
+    nextEl: ".gallery__arrow-next",
+    prevEl: ".gallery__arrow-prev",
+  },
+  loop: true,
+  watchOverflow: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".gallery__pagination",
     clickable: true,
   },
-
+  autoHeight: false,
   breakpoints: {
-    320: {
+    0: {
       slidesPerView: 1,
-      spaceBetween: 20,
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
     },
     768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+      slidesPerView: 3,
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
     },
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+    1920: {
+      slidesPerView: 5,
     },
   },
 });
--- templates/home.php
+++ templates/home.php
@@ -108,45 +108,38 @@ echo file_get_contents($icon_arrow);
         <div class="container">
             <h2 class="gallery__title title">
                 <?php the_field('gallery__title'); ?>
-            </h2>
+            </h2></div>
+        <div class="container-gallery">
             <div class="gallery-slider swiper">
-                <?php 
-                $rows = get_field('gallery__item', 'option');
-                if( $rows ) { 
-                  echo '<div class="gallery__list swiper-wrapper">';
-                    foreach( $rows as $row ) { 
-                        $galleryImg = $row['gallery__img'];
-          
-                        echo '<div class="swiper-slide">';  
-                        
-          echo '<div class="gallery__img">';
-          echo wp_get_attachment_image( $galleryImg, 'full' );
-          echo '</div>';
-          echo '<div>';          
-                    } 
-                } 
-                ?>
-                </div>
-            <?php
+                <?php
             if (have_rows('events__item')): ?>
-                <ul class="gallery__list ">
+                <ul class="gallery__list swiper-wrapper">
                     <?php while (have_rows('gallery__item')):
                         the_row(); ?>
-                        <li class="gallery__item">
+                        <li class="gallery__item swiper-slide">
                             <div class="gallery__img">
                                 <img src="<?php the_sub_field('gallery__img'); ?>" alt="gallery" />
                             </div>
-
                         </li>
                     <?php endwhile; ?>
                 </ul>
             <?php endif; ?>
+                </div>
+                        </div>
+            <div class="container">
+            <div class="gallery__arrows">
+      <button class="gallery__arrow-prev"></button>
+      <div class="gallery__pagination"></div>  
+<button class="gallery__arrow-next"></button>
+</div>
+</div>
 
-        </div>
+
+
     </section>
     <section class="section partners" id="partners">
+        <div class="partners__line line"></div>
         <div class="container">
-            <div class="partners__line line"></div>
             <h2 class="partners__title title">
                 <?php the_field('partners__title'); ?>
             </h2>
@@ -159,9 +152,21 @@ echo file_get_contents($icon_arrow);
                             <div class="partners__img">
                                 <img src="<?php the_sub_field('partners__img'); ?>" alt="partners" />
                             </div>
-                            <p class="partners__name">
-                                <?php the_field('partners__name'); ?>
-                            </p>
+                            <div class="partners__name">
+                                <?php
+            $link = get_sub_field('partners__name');
+            if ($link):
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                    <a  href="<?php echo esc_url($link_url); ?>"
+                        target="<?php echo esc_attr($link_target); ?>">
+                        <?php echo esc_html($link_title); ?>
+                    </a>
+            <?php endif; ?>
+                                
+                    </div>
                         </li>
                     <?php endwhile; ?>
                 </ul>
@@ -169,28 +174,82 @@ echo file_get_contents($icon_arrow);
         </div>
     </section>
     <section class="section docs" id="documents">
+        <div class="docs__line line"></div>
         <div class="container">
-            <div class="docs__line line"></div>
-            <h2 class="docs__title"></h2>
-            <ul class="docs__list">
-                <li class="docs__item">
-                    <div class="docs__name"></div>
-                </li>
-            </ul>
+            <h2 class="docs__title"><?php the_field('docs__title'); ?></h2>
+            <?php
+            if (have_rows('docs__item')): ?>
+                <ul class="docs__list">
+                    <?php while (have_rows('docs__item')):
+                        the_row(); ?>
+                        <li class="docs__item">
+                                <?php
+            $link = get_sub_field('docs__name');
+            if ($link):
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                    <a class="docs__name" href="<?php echo esc_url($link_url); ?>"
+                        target="<?php echo esc_attr($link_target); ?>">
+                        <?php echo esc_html($link_title); ?>
+                        <?php
+$open_arrow = get_stylesheet_directory() . '/assets/images/open.svg';
+echo file_get_contents($open_arrow);
+?>
+                    </a>
+            <?php endif; ?>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
         </div>
     </section>
     <section class="section contacts" id="contacts">
+        <div class="contacts__line line"></div>
         <div class="container">
-            <div class="contacts__line line"></div>
-            <h2 class="contacts__title"></h2>
-            <div class="contacts__map"></div>
+            <h2 class="contacts__title"><?php the_field('contacts__title'); ?></h2>
+            <div class="contacts__map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10605.465916880457!2d22.3937751!3d48.3534812!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4738f9542fcb2553%3A0x3775878ed6531bd!2z0JHQtdGA0LXQs9GW0LLRgdGM0LrQuNC5INCa0KXQnw!5e0!3m2!1suk!2sua!4v1701273286165!5m2!1suk!2sua" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="contacts__border"></div>
             <ul class="contacts__list">
                 <li class="contacts__item">
-                    <div class="contacts__img"></div>
-                    <h3 class="contacts__name"></h3>
-                    <p class="contacts__text"></p>
+                    <div class="contacts__address-img">
+                <img src="<?php the_field('contacts__address-img'); ?>" alt="address" />
+                </div>
+                    <h3 class="contacts__address-title"><?php the_field('contacts__address-title'); ?></h3>
+                    <?php
+            $link = get_field('contacts__address-text');
+            if ($link):
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                    <a class="contacts__address-text" href="<?php echo esc_url($link_url); ?>"
+                        target="<?php echo esc_attr($link_target); ?>">
+                        <?php echo esc_html($link_title); ?>
+                    </a>
+            <?php endif; ?>
                 </li>
-            </ul>
+                <li class="contacts__item">
+                    <div class="contacts__email-img">
+                        <img src="<?php the_field('contacts__email-img'); ?>" alt="email" />
+                    </div>
+                    <h3 class="contacts__email-title"><?php the_field('contacts__email-title'); ?></h3>
+                    <p class="contacts__email-text"><?php the_field('contacts__email-text'); ?></p>
+                </li>
+                <li class="contacts__item">
+                    <div class="contacts__phone-img">
+                         <img src="<?php the_field('contacts__phone-img'); ?>" alt="phone" />
+                    </div>
+                    <h3 class="contacts__phone-title"><?php the_field('contacts__phone-title'); ?></h3>
+                    <div>
+                    <p class="contacts__phone-text"><?php the_field('contacts__phone-text'); ?></p>
+                    <p class="contacts__phone-number"><?php the_field('contacts__phone-number'); ?></p>
+                    </div>
+                </li>
+                </ul>
         </div>
     </section>
 </main>
