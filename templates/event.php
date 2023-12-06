a1a459ebba802de168cd013bc3bd6b650a86c30d
NilKad <kadulin@gmail.com>
2023-12-06 02:44:37 +0100

    slider eample photo

--- templates/event.php
+++ templates/event.php
@@ -36,27 +36,148 @@ get_header();
     </div>
 
     <div class="container-gallery">
-        <div class="gallery-wraper">
-            <div class="gallery-sm left-1">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/event.jpg" class='gallery-img' alt="photo">
+        <div class="gallery-wrapper">
+
+            <div class="slider0 swiper">
+                <div class="image-slider__wrapper swiper-wrapper">
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-01.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-02.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-03.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-04.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-05.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-06.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-07.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-08.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-09.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-10.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-11.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-12.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-13.jpg" class='gallery-img' alt="photo">
+                    </div>
+                </div>
             </div>
-            <div class="gallery-sm left">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/event.jpg" class='gallery-img' alt="photo">
+
+            <div class="slider1 swiper">
+                <div class="image-slider__wrapper swiper-wrapper">
+                <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-01.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-02.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-03.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-04.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-05.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-06.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-07.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-08.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-09.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-10.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-11.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-12.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-13.jpg" class='gallery-img' alt="photo">
+                    </div>
+                </div>
             </div>
-            <div class="gallery-lg center">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/event.jpg" class='gallery-img' alt="photo">
+
+
+
+            <div class="slider2 swiper">
+                <!-- <div class="image-slider__wrapper swiper-wrapper"></div> -->
+                <div class="image-slider__wrapper swiper-wrapper">
+                <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-01.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-02.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-03.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-04.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-05.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-06.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-07.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-08.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-09.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-10.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-11.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-12.jpg" class='gallery-img' alt="photo">
+                    </div>
+                    <div class="image-slider__slide swiper-slide">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/examples/f-13.jpg" class='gallery-img' alt="photo">
+                    </div>
+                </div>
             </div>
-            <div class="gallery-sm right">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/event.jpg" class='gallery-img' alt="photo">
-            </div>
-            <div class="gallery-sm right-1">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/event.jpg" class='gallery-img' alt="photo">
-            </div>
+
         </div>
 
         <div class="gallery-paginate">
             <div class="gallery-nav">
-                <button typpe="button" class='nav-btn'>
+                <button typpe="button" class='nav-btn swiper-button-prev'>
                     <svg class="gallery-arrow">
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/icons.svg#icon-previous-btn" alt="arrow left"></use>
                     </svg>
@@ -66,12 +187,14 @@ get_header();
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/icons.svg#icon-nav-dots" alt="nav dots"></use>
                     </svg>
                 </button>
-                <button typpe="button" class='nav-btn'>
+                <button typpe="button" class='nav-btn swiper-button-next'>
                     <svg class="gallery-arrow">
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/icons.svg#icon-next-btn" alt="arrow right"></use>
                     </svg>
                 </button>
             </div>
+            <!-- <div class="swiper-button-prev"></div> -->
+            <!-- <div class="swiper-button-next"></div> -->
         </div>
     </div>
 
