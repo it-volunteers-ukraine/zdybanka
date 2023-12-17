aea8ea411a15cfc8b1c041cf5f581343c67426e4
NilKad <kadulin@gmail.com>
2023-12-17 19:54:10 +0100

    add text btn more-less

--- src/scripts/parts-scripts/gallery.js
+++ src/scripts/parts-scripts/gallery.js
@@ -134,11 +134,4 @@ window.addEventListener("resize", (e) => {
 //   console.log("!!!!resize");
 // });
 
-console.log("event page");
-const btnMoreRef = document.getElementById("btn-more");
-const textContentRef = document.querySelector(".event-content");
 
-btnMoreRef.addEventListener("click", (e) => {
-  textContentRef.classList.toggle("clamp");
-  textContentRef.scrollIntoView({ behavior: "smooth" });
-});
