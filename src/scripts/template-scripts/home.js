95c3bd19883c786d4c190fd0c4725d618ab9f28c
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-12-29 09:26:43 +0200

    files bug fixed

--- src/scripts/template-scripts/home.js
+++ src/scripts/template-scripts/home.js
@@ -9,6 +9,27 @@ list.addEventListener("click", (e) => {
   if (parentEl.classList.contains(exp)) {
     parentEl.classList.remove(exp);
   } else {
+    let docs_doc_element = parentEl.querySelector("iframe");
+
+    if (docs_doc_element.getAttribute("src") == null) {
+      docs_doc_element.setAttribute(
+        "src",
+        docs_doc_element.getAttribute("data-src")
+      );
+    }
     parentEl.classList.add(exp);
   }
 });
+
+const partners = document.querySelectorAll(".partners__item");
+const partnersList = document.querySelector(".partners__list");
+const partnersCount = partners.length;
+if (partnersCount % 5 === 0) {
+  partnersList.classList.add("grid-five");
+} else if (partnersCount % 4 === 0) {
+  partnersList.classList.add("grid-four");
+} else if (partnersCount > 3 && partnersCount % 3 === 0) {
+  partnersList.classList.add("grid-three");
+} else {
+  partnersList.classList.add("grid-four");
+}
